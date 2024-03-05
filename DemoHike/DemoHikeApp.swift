928c7c052b84b643d3a64b4aaa3a74f39375916a
cesar <cursos.mapatupisaro@gamail.com>
2024-03-05 17:35:36 -0600

    Initial Commit

---
+++ DemoHike/DemoHikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoHikeApp.swift
+//  DemoHike
+//
+//  Created by Aguid Ramirez Sanchez on 05/03/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoHikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
